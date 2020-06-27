e8fcbd570f04b1f0ab0176de56a70a28f9c3925f
shristibansal <shristibansal6@gmail.com>
2020-06-27 10:30:37 +0530

    Task 4

--- Python-Task4/question2/main.py
+++ Python-Task4/question2/main.py
@@ -5,12 +5,12 @@ import list_functions
 import string_functions
 import operator_functions
 
-#Declaring an empty list 
+# Declaring an empty list 
 list = []
 
-#Taking input from the user
-size = int(input("Enter the number of elements: "))
-for index in range (0, size):
+# Taking input from the user
+listSize = int(input("Enter the number of elements: "))
+for index in range (0, listSize):
         element = float(input("Enter the list element"))
         list.append(element)
 
@@ -18,43 +18,45 @@ for index in range (0, size):
 square = list_functions.calculateSquare(list[0])
 print("Square of first element of list is ",square)
 
-maximum = list_functions.findMaximum(list, size)
-print("Maximum element of list is ",maximum)
+listMaximum = list_functions.findMaximum(list, listSize)
+print("Maximum element of list is ",listMaximum)
 
-minimum = list_functions.findMinimum(list, size)
-print("Minimum element of list is ",minimum)
+listMinimum = list_functions.findMinimum(list, listSize)
+print("Minimum element of list is ",listMinimum)
 
-sum = list_functions.findSum(list, size)
-print("Sum of elements of list is ",sum)
+listSum = list_functions.findSum(list, listSize)
+print("Sum of elements of list is ",listSum)
 
 print()
 
-#Taking input from the user
+# Taking input from the user
 string1 = input("Enter first string: ")
 string2 = input("Enter second string: ")
 
 # Using string_functions
-length1 = string_functions.findLength(string1)
-length2 = string_functions.findLength(string2)
-print("Length of first string: ",length1)
-print("Length of second string: ",length2)
+stringLength1 = string_functions.findLength(string1)
+stringLength2 = string_functions.findLength(string2)
+print("Length of first string: ",stringLength1)
+print("Length of second string: ",stringLength2)
 
-midChar = string_functions.findMiddle(string1, length1)
+midChar = string_functions.findMiddle(string1, stringLength1)
 print("Middle character of first string: ",midChar)
 
-vowels = string_functions.countVowels(string1)
-print("Number of vowels in first string: ",vowels)
+no_of_Vowels = string_functions.countVowels(string1)
+print("Number of vowels in first string: ",no_of_Vowels)
 
-words = string_functions.countWords(string2, length2)
-print("Number of words in second string: ",words)
+no_of_Words = string_functions.countWords(string2, stringLength2)
+print("Number of words in second string: ",no_of_Words)
 
-#Taking input from the user
+# Taking input from the user
 value1 = int(input("Enter first number: "))
 value2 = int(input("Enter second number: "))
 
 # Using operator_functions
 operator_functions.operatorAnd(value1, value2)
+
 operator_functions.operatorOr(value1, value2)
+
 operator_functions.operatorNot(value1)
 
 
